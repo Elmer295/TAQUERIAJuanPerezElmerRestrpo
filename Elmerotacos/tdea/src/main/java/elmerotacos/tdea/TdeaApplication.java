f91041c60abad44c359a7c90dd45b01a9fd9a4c6
Elmer295 <restrepoelmer12@gmail.com>
2025-12-03 12:23:38 -0500

    first commit

---
+++ Elmerotacos/tdea/src/main/java/elmerotacos/tdea/TdeaApplication.java
@@ -0,0 +1,15 @@
+package elmerotacos.tdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdeaApplication.class, args);
+	System.out.println("se corrio correctamente");
+	}
+
+
+}
---
+++ Elmerotacos/tdea/src/main/java/elmerotacos/tdea/controller/IndexController.java
@@ -0,0 +1,40 @@
+package elmerotacos.tdea.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    
+    
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/mision")
+    public String mision() {
+        return "mision";
+    }
+
+    @GetMapping("/vision")
+    public String vision() {
+        return "vision";
+    }
+
+    @GetMapping("/objetivo")
+    public String objetivo() {
+        return "objetivo";
+    }
+
+
+
+
+    
+}
